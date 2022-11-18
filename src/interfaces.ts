a8a7b11f27bde388245cb91929d42d4973b3043f
siddiqahmed882 <siddiqahmed882@gmail.com>
2022-11-18 13:00:37 +0500

    ...

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,4 +1,11 @@
 export interface PageBannerProps {
   title: string,
-  imageUrl?: string
+  imageUrl?: string;
+}
+
+export interface CarouselSlideProps {
+  heading: string;
+  lead: string;
+  redirect: string;
+  bgImage: string;
 }
